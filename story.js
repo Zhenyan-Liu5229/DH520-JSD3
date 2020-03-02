3e38dde1bc4a521a00ffbf921fd9cddd50ead179
Zhenyan Liu <Zhenyan-Liu5229@users.noreply.github.com>
2020-03-02 09:22:34 -0700

    Update story.js

--- story.js
+++ story.js
@@ -1,5 +1,7 @@
 var opening = document.getElementById('opening').innerHTML;
 var one = document.getElementById('one').innerHTML;
+var two = document.getElementById('two').innerHTML;
+
 var pet_name = 'Tom', slayer_name = 'Bobalat'
 
 
@@ -15,28 +17,33 @@ function dogName() {
 
 
 function proceed(scene) {
-	if (scene == d){
+	if (scene == d) {
 		if (event.keyCode == 68 ||event.keyCode == 68+32) {
   	    document.getElementById('one').style.display = 'block';
   	    document.getElementById('opening').style.display = 'none';
         } else {
   	        alert('Please press d to continue!')
         }
-	} else if (scene == h){
+	} else if (scene == h) {
 		if (event.keyCode == 72 ||event.keyCode == 72+32) {
-  	    document.getElementById('two').style.display = 'block';
-  	    document.getElementById('one').style.display = 'none';
+      	    document.getElementById('two').style.display = 'block';
+      	    document.getElementById('one').style.display = 'none';
         } else {
   	        alert('Please press h to continue!')
         }
-    } 
+    } else if (scene == i) {
+        document.getElementById('items').style.display = 'block';
+        document.getElementById('two').style.display = 'none';
+    }
     
 }
 
 
 function playerName() {
 	var player_name = prompt('What\'s your name?');
+    var first_three = player_name.slice(0,3);
 	if (player_name != null || player_name != '') {
         document.getElementById('one').innerHTML = one.replace('Bobalat', player_name);
+        document.getElementById('two').innerHTML = two.replace('Bobalat', player_name).replace(/Bob\b/g, first_three);
 	}
 }
